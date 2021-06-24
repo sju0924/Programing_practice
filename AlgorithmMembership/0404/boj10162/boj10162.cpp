a24dc7e49faf07895001162d9c8fecb625046ab6
ZION <sju0924@khu.ac.kr>
2021-06-24 23:59:25 +0900

    Create boj10162.cpp

---
+++ AlgorithmMembership/0404/boj10162/boj10162.cpp
@@ -0,0 +1,23 @@
+﻿// boj10162.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int A, B, C, D, E, res = 0;
+	cin >> A >> B >> C >> D >> E;
+
+	if (A <= 0) {
+		res = -C * A + D + B*E;
+	}
+	else {
+		res = (B - A) * E;
+	}
+	cout << res;
+
+
+}
